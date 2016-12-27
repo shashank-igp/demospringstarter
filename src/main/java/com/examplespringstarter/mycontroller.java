a20a13a0172b7fce352675a4085ba1578683828d
shashank-igp <shashank.sharma@igp.com>
2016-12-27 18:36:23 +0530

    Initial commit

---
+++ src/main/java/com/examplespringstarter/mycontroller.java
@@ -0,0 +1,20 @@
+package com.examplespringstarter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by shanky on 27/12/16.
+ */
+@Controller
+@RequestMapping(value = "/myhome")
+public class mycontroller {
+
+    @RequestMapping(value = "/{a}/{b}")
+    public @ResponseBody double sum (@PathVariable double a, @PathVariable double b){
+        return a+b;
+    }
+}
